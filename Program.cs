df6d3d06f5a85e20d427e217e3d2b7301f0dfaac
Matheus Bento <matheus.th.bento@gmail.com>
2025-02-03 20:18:34 -0300

    Setting up the API to work with snake case request parameters

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder
+    .Services
+        .AddControllers()
+        .AddJsonOptions(options => {
+            options.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.SnakeCaseLower;
+            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
+        });
 
 var app = builder.Build();
 
